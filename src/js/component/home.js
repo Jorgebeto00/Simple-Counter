45205d47f047b14d9cf939f52fe1c04c85f0a713
Jorge R <jorgebeto00@protonmail.com>
2021-03-06 23:11:54 +0000

    added logic and some css

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,25 +1,23 @@
-import React from "react";
+// import React from "react";
+// import PropTypes from "prop-types";
 
-//include images into your bundle
+// //include images into your bundle
 
+// //create your first component
+// function Home(props) {
+// 	return (
+// 		<div className="counter">
+// 			<div className="clockIcon">
+// 				<i className="far fa-clock"></i>
+// 			</div>
+// 			<div className="seconds">0</div>
+// 			<div className="seconds">0</div>
+// 			<div className="seconds">0</div>
+// 			<div className="seconds">0</div>
+// 			<div className="seconds">0</div>
+// 			<div className="seconds">0</div>
+// 		</div>
+// 	);
+// }
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				Hi
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-}
-
+// export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -8,8 +9,24 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+function Counter(props) {
+	return (
+		<div className="counter">
+			<div className="clockIcon">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="six">{props.digitOne}</div>
+			<div className="five">{props.digitFive}</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="three">{props.digitThree}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="one">{props.digitOne}</div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let time = 0;
+setInterval(function() {
+	ReactDOM.render(<Counter />, document.querySelector("#app"));
+	time++;
+}, 1000);
